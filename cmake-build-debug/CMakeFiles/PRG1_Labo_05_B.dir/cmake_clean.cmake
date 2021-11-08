9fe038a99d96580bb4b09fce2ea6ec6d29102c7c
ibex17 <richard.sierra@outlook.com>
2021-11-08 17:20:09 +0100

    first

---
+++ cmake-build-debug/CMakeFiles/PRG1_Labo_05_B.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PRG1_Labo_05_B.dir/main.cpp.o"
+  "PRG1_Labo_05_B"
+  "PRG1_Labo_05_B.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PRG1_Labo_05_B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PRG1_Labo_05_B.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PRG1_Labo_05_B.
+# This may be replaced when dependencies are built.
